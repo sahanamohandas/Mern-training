fa13c87c7d7a9eaa1644f89fea1a9bf951fb5a1a
sahanamohandas <sahanamohandas123@gmail.com>
2025-06-19 16:34:02 +0530

    first

---
+++ js.basic.js
@@ -0,0 +1,19 @@
+let a=10;//number
+let name="tamil";//string
+let isbool=true;//boolean
+let c;//undifined
+let b=null;//null
+let abc=2345678908765;
+let age=15;
+if(age>18){
+    console.log("yes true")
+}
+else{
+    console.log("false")
+}
+if(age>18){
+    console.log("yes true")
+}
+else if(5>age<10){
+    console.log("s")
+}
